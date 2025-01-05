f030366385676bcffa5ad1305f3c01a34d2a9bf2
dev-reyyy <dev.reyyy@gmail.com>
2025-01-05 17:15:52 +0800

    Added New Sidebar.
    Created a new sidebar component. Added icon assets.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,9 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Default Title')</title>
 
+    {{-- Flat Icons --}}
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
 </head>
+
 <body>
-    @yield('content')
+    <x-sidebar></x-sidebar>
+    
+    <main>
+        @yield('content')
+    </main>
 </body>
+
 </html>
