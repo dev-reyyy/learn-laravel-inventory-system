4a2880a4e9243e73dbf5c5cbe3490052a2cc91be
dev-reyyy <dev.reyyy@gmail.com>
2025-01-12 23:17:14 +0800

    Install Bootstrap.
    Added bootstrap to the project via npm install instead of through cdn.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,14 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Default Title')</title>
 
-    {{-- Bootstrap --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     {{-- Bootstrap Icon --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     {{-- DataTables --}}
     <link rel="stylesheet" href="https://cdn.datatables.net/2.2.0/css/dataTables.dataTables.min.css">
     {{-- Custom CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite('resources/css/app.css')
+    
 </head>
 
 <body>
@@ -35,12 +34,10 @@
     
     {{-- JQuery --}}
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    {{-- Bootstrap JS --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     {{-- DataTables JS --}}
     <script src="https://cdn.datatables.net/2.2.0/js/dataTables.min.js"></script>
     {{-- Custom JS --}}
-    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+    @vite('resources/js/app.js')
 </body>
 
 </html>
